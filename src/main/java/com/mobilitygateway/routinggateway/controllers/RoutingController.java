6b1d3612228bd76efa3672cd1721f4d0fac9278c
Fabian <fabian_leibinger@gmx.de>
2021-08-23 21:21:28 +0200

    added http endpoint for routing requests, removed models for routing gateway

--- src/main/java/com/mobilitygateway/routinggateway/controllers/RoutingController.java
+++ src/main/java/com/mobilitygateway/routinggateway/controllers/RoutingController.java
@@ -3,6 +3,7 @@ package com.mobilitygateway.routinggateway.controllers;
 import com.mobilitygateway.exceptions.BadGatewayException;
 import com.mobilitygateway.routinggateway.HttpRoutingGateway;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -13,24 +14,43 @@ import java.util.Optional;
 @RestController
 @RequestMapping("routing")
 public class RoutingController {
-    //Path of the related controller at the upstream routing gateway.
-    private static final String PATH_SEGMENT = "routingServices/names";
 
     /**
      * Returns the names of the available routing services or throws BadGatewayException.
      *
-     * @return nameList
+     * @return nameList json
      */
     @GetMapping(path = "services")
     @ResponseStatus(HttpStatus.OK)
     public static String getRoutingServices() {
         HttpRoutingGateway httpService = new HttpRoutingGateway();
-        Optional<String> responseBody = httpService.getRequest(PATH_SEGMENT);
+        Optional<String> responseBody = httpService.getRequest("routingServices/names");
         if (responseBody.isPresent()) {
             System.out.println("Received routing service names successfully.");
             return responseBody.get();
         } else {
             throw new BadGatewayException("Failed to receive routing service names.");
+        }
+    }
+
+    /**
+     * Returns routes for a routing request or throws BadGatewayException.
+     *
+     * @param routingRequest json
+     * @return routingResponse json
+     */
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public static String receiveRoutes(String routingRequest) {
+        HttpRoutingGateway httpService = new HttpRoutingGateway();
+        Optional<String> responseBody = httpService.postRequest("routing", routingRequest);
+        if (responseBody.isPresent()) {
+            System.out.println("Received routes successfully");
+            return responseBody.get();
+        } else {
+            throw new BadGatewayException("Failed to receive routes.");
         }
     }
 }
